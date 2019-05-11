d1173d49a9fd952bcdf10bc295d23b5cea2fb74b
unknown <nikoladimitrijevic1991@gmail.com>
2019-05-11 14:28:31 +0200

    dropdowns

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,51 @@ for (var i = 0; i < productBox.length; i++) {
     e.target.childNodes[1].childNodes[5].style.bottom = '-2.5vw';
   });
 }
+// registration
+var register = document.getElementById('register');
+var buyerSellerBox = document.getElementById('buyerSellerBox');
+
+register.addEventListener('mouseenter',()=>{
+  buyerSellerBoxShow();
+  console.log('enter');
+});
+
+register.addEventListener('mouseleave',()=>{
+  buyerSellerBoxHide();
+  console.log('leave');
+});
+
+function buyerSellerBoxShow() {
+  buyerSellerBox.style.height = "13vw";
+  buyerSellerBox.style.bottom = "-13vw";
+}
+
+function buyerSellerBoxHide() {
+  buyerSellerBox.style.height = "0vw";
+  buyerSellerBox.style.bottom = "0vw";
+}
+
+// sellSomething sellerOnly
+
+var sellSomething = document.getElementById('sellSomething');
+var sellerOnly = document.getElementById('sellerOnly');
+
+sellSomething.addEventListener('mouseenter',()=>{
+  SellerBoxShow();
+  console.log('enter');
+});
+
+sellSomething.addEventListener('mouseleave',()=>{
+  SellerBoxHide();
+  console.log('leave');
+});
+
+function SellerBoxShow() {
+  sellerOnly.style.height = "12.56vw";
+  sellerOnly.style.bottom = "-13vw";
+}
+
+function SellerBoxHide() {
+  sellerOnly.style.height = "0vw";
+  sellerOnly.style.bottom = "0vw";
+}
