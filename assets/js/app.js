79bc98a572db7a0b0703b2c783d17ff6cb544560
unknown <nikoladimitrijevic1991@gmail.com>
2019-05-13 16:05:18 +0200

    mega change

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,9 @@ var productBox = document.getElementsByClassName('productBox');
 for (var i = 0; i < productBox.length; i++) {
   productBox[i].addEventListener('mouseenter',(e)=>{
     e.target.childNodes[5].style.maxHeight= '10vw';
-    console.log(e.target.childNodes[1].childNodes[5]);
+    // console.log(e.target.childNodes[1].childNodes[5]);
     e.target.childNodes[1].childNodes[3].style.opacity = '1';
-    e.target.childNodes[1].childNodes[3].style.top = '0.2932vw';
+    e.target.childNodes[1].childNodes[3].style.top = '2.5vw';
     e.target.childNodes[1].childNodes[5].style.bottom = '0vw';
   });
   productBox[i].addEventListener('mouseleave',(e)=>{
