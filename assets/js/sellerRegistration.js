af5eaa62f0c2fd49449adcf5c74a75cdff474874
unknown <nikoladimitrijevic1991@gmail.com>
2019-05-15 18:32:11 +0200

    seller reg mobile response

--- assets/js/sellerRegistration.js
+++ assets/js/sellerRegistration.js
@@ -11,22 +11,13 @@ for (var i = 0; i < regClassBox.length; i++) {
 }
 
 function titleShow(e) {
-  e.target.childNodes[3].style.bottom = '0vw';
   e.target.childNodes[3].style.opacity = '1';
 }
 
 function titleHide(e){
-  e.target.childNodes[3].style.bottom = '-3.4vw';
-  e.target.childNodes[3].style.opacity = '0';
+  e.target.childNodes[3].style.opacity = '0.7';
 }
 
-// function rotationStartAnim() {
-//   for (var i = 0; i < regClassBox.length; i++) {
-//     setTimeout(function(){
-//       console.log(i);
-//       },2000)
-//   }
-// }
 var myi = 0;
 function myLoop() {
   setTimeout(function () {
