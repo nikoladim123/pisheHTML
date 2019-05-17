b1fc35557866b4b19836aa46f46fd06a75efea6b
unknown <nikoladimitrijevic1991@gmail.com>
2019-05-17 16:18:24 +0200

    fruits accordium

--- assets/js/product.js
+++ assets/js/product.js
@@ -8,17 +8,35 @@ for (var i = 0; i < categoryList.length; i++) {
     closeAllCategoryAccordium()
   });
 
-
   categoryList[i].addEventListener('click',(e)=>{
     e.target.childNodes[1].style.maxHeight = '10vw';
   });
-
-
-
 }
 
 function closeAllCategoryAccordium() {
   for (var i = 0; i < categoryList.length; i++) {
     categoryList[i].childNodes[1].style.maxHeight = '0vw';
   }
+}
+
+// lists fixed
+var lists = document.getElementsByClassName('lists');
+var colOneHolder = document.getElementsByClassName('colOneHolder');
+
+function fixedLists() {
+  if(colOneHolder[0].getBoundingClientRect().y < window.innerHeight /100 *2){
+    lists[0].style.position = 'fixed';
+    lists[0].style.top = '2vh';
+  }else if(colOneHolder[0].getBoundingClientRect().y > window.innerHeight /100 *2){
+    lists[0].style.position = 'relative';
+    lists[0].style.top = '0vw';
+  }
+  // lists[0].getBoundingClientRect().y;
+}
+
+// win funcitons
+// win funcitons
+// win funcitons
+window.onscroll = ()=>{
+  fixedLists();
 }
