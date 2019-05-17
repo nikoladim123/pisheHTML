48abda3cbdfdd5d2290019e9c3155623a11e0044
unknown <nikoladimitrijevic1991@gmail.com>
2019-05-17 19:48:04 +0200

    fixed heart and shit landing page

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,6 +45,7 @@ var love = document.getElementsByClassName('love');
 for (var i = 0; i < imgShareLikeCallDivIcon.length; i++) {
   imgShareLikeCallDivIcon[i].addEventListener('click',(e)=>{
     e.target.childNodes[3].style.opacity = '1';
+    console.log(e.target.childNodes[3]);
   })
 }
 
@@ -53,15 +54,16 @@ for (var i = 0; i < imgShareLikeCallDivIcon.length; i++) {
 // share icon socialMediaDropDown
 // var imgShareLikeCallDivIcon = document.getElementsByClassName('imgShareLikeCallDivIcon')[0];
 var socialMediaDropDown = document.getElementsByClassName('socialMediaDropDown')[0];
+var shareDropDown = document.getElementsByClassName('shareDropDown');
 
-for (var i = 0; i < imgShareLikeCallDivIcon.length; i++) {
-  imgShareLikeCallDivIcon[i].addEventListener('mouseenter',(e)=>{
+for (var i = 0; i < shareDropDown.length; i++) {
+  shareDropDown[i].addEventListener('mouseenter',(e)=>{
     e.target.childNodes[3].style.display ='flex';
     e.target.childNodes[3].style.opacity = '1';
     console.log(e.target.childNodes);
   });
 
-  imgShareLikeCallDivIcon[i].addEventListener('mouseleave',(e)=>{
+  shareDropDown[i].addEventListener('mouseleave',(e)=>{
     e.target.childNodes[3].style.opacity = '0';
     setTimeout(function () {
       e.target.childNodes[3].style.display ='none';
@@ -70,14 +72,14 @@ for (var i = 0; i < imgShareLikeCallDivIcon.length; i++) {
 }
 
 
-imgShareLikeCallDivIcon[0].addEventListener('mouseenter',()=>{
-  socialMediaDropDown.style.display ='flex';
-  socialMediaDropDown.style.opacity = '1';
-});
-
-imgShareLikeCallDivIcon[0].addEventListener('mouseleave',()=>{
-  socialMediaDropDown.style.opacity = '0';
-  setTimeout(function () {
-    socialMediaDropDown.style.display ='none';
-  }, 300);
-});
+// imgShareLikeCallDivIcon[0].addEventListener('mouseenter',()=>{
+//   socialMediaDropDown.style.display ='flex';
+//   socialMediaDropDown.style.opacity = '1';
+// });
+//
+// imgShareLikeCallDivIcon[0].addEventListener('mouseleave',()=>{
+//   socialMediaDropDown.style.opacity = '0';
+//   setTimeout(function () {
+//     socialMediaDropDown.style.display ='none';
+//   }, 300);
+// });
