4edc08fc11667e4a9db07d6c16b2a08b1e692f42
unknown <nikoladimitrijevic1991@gmail.com>
2019-05-20 17:59:29 +0200

    fixed much homepage

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -83,3 +83,94 @@ function deleteLastImage() {
 function opacityLastImage() {
   leftSliderBox.children[2].style.opacity = '0';
 }
+
+// right slider
+// right slider
+// right slider
+// right slider
+// right slider
+var iterTwo = 1;
+function setDivTwo() {
+  prodBoxTwoInnerHTML =`
+    <div class="productImageDiv">
+      <img class="productImage" src="assets/images/jewelery/${iterTwo % 8}.jpg" alt="">
+      <div class="imgShareLikeCallDiv">
+        <div class="imgShareLikeCallDivIcon shareDropDown">
+          <img class="share-icon" src="assets/images/share.png" alt="">
+
+            <div class=" socialMediaDropDown">
+              <img class="fb-icon" src="assets/images/fb-icon.png" alt="">
+              <img class="twitter-icon" src="assets/images/twitter-icon.png" alt="">
+              <img class="instagram-icon" src="assets/images/instagram-icon.png" alt="">
+              <img class="linkedIn-icon" src="assets/images/linkedIn-icon.png" alt="">
+            </div>
+
+        </div>
+        <div class="imgShareLikeCallDivIcon">
+          <img class="call-icon" src="assets/images/call-icon.png" alt="">
+        </div>
+        <div id="loveFullContainer" class="imgShareLikeCallDivIcon">
+          <img class="love loveEmpty" src="assets/images/love.png" alt="">
+          <img id="loveFull" class="love loveFull" src="assets/images/loveFull.png" alt="">
+        </div>
+      </div>
+      <div class="clientRatingBox">
+        <p>Seller rating</p>
+        <img class="sellerRating" src="assets/images/clientRating.png" alt="client rating">
+        <img class="sellerCard" src="assets/images/card.png" alt="shoping cart">
+      </div>
+    </div>
+    <div class="itemDescription">
+      <!-- <img class="stars" src="assets/images/productImages/stars.jpg" alt=""> -->
+      <p class="itemDescriptionText" >Lorem isspsum Lorem ipsum Lorem  Lorem ipsum</p>
+      <p class="productPrice">$ 75.00</p>
+    </div>
+    <div class="itemTags">
+      <div class="tag">
+        WATCH
+      </div>
+      <div class="tag">
+        GOLD
+      </div>
+      <div class="tag">
+        WRIST
+      </div>
+      <div class="tag">
+        JEWELERY
+      </div>
+    </div>
+  `;
+}
+var prodBoxTwoInnerHTML;
+
+
+var nextButtonTwo = document.getElementsByClassName('nextButton')[1];
+var rightSliderBox = document.getElementsByClassName('rightSliderBox')[0];
+
+
+nextButtonTwo.addEventListener('click',()=>{
+  opacityLastImageTwo();
+  setTimeout(function () {
+    setDivTwo();
+    deleteLastImageTwo();
+    insertFirstImageTwo();
+    addProdBoxListeners();
+    iterTwo++;
+  }, 400);
+})
+
+
+function insertFirstImageTwo() {
+  var newDiv = document.createElement('div');
+  newDiv.className = 'productBox';
+  newDiv.innerHTML = prodBoxTwoInnerHTML;
+  rightSliderBox.insertAdjacentElement('afterbegin', newDiv);
+}
+
+function deleteLastImageTwo() {
+  rightSliderBox.removeChild(rightSliderBox.children[2]);
+}
+
+function opacityLastImageTwo() {
+  rightSliderBox.children[2].style.opacity = '0';
+}
